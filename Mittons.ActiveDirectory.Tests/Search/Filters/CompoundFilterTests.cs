f9cac335e7ad4142e2e4e55f87fa5e893655f4be
ktmitton <kdrive113@gmail.com>
2025-10-18 21:32:18 -0400

    Add Compound filter

---
+++ Mittons.ActiveDirectory.Tests/Search/Filters/CompoundFilterTests.cs
@@ -0,0 +1,111 @@
+using Mittons.ActiveDirectory.Search;
+using Mittons.ActiveDirectory.Search.Filters;
+using Mittons.ActiveDirectory.Tests.Data;
+using Moq;
+
+namespace Mittons.ActiveDirectory.Tests.Search.Filters;
+
+public class CompoundFilterTests
+{
+    [Test]
+    [MatrixDataSource]
+    public async Task Ctor_WhenCreated_ExpectPropertiesToBeSet(
+        [MatrixMethod<ComponentDataDatasource>(nameof(ComponentDataDatasource.CompoundLogicalOperatorsDatasource))] ComponentData<LogicalOperator> logicalOperatorComponent,
+        [MatrixMethod<CompoundFilterTests>(nameof(RandomFiltersDatasource))] (Mock<IFilter> filter, string defaultString, string directoryServicesString, string ldapString)[] filters
+    )
+    {
+        // Arrange
+        // Act
+        CompoundFilter compoundFilter = new(logicalOperatorComponent.Component, filters.Select(f => f.filter.Object));
+
+        // Assert
+        await Assert.That(compoundFilter.LogicalOperator).IsEqualTo(logicalOperatorComponent.Component);
+        await Assert.That(compoundFilter.Filters).IsEquivalentTo(filters.Select(f => f.filter.Object));
+    }
+
+    [Test]
+    [MatrixDataSource]
+    public void Ctor_WhenCreatedWithASimpleOperator_ExpectException(
+        [MatrixMethod<ComponentDataDatasource>(nameof(ComponentDataDatasource.SimpleLogicalOperatorsDatasource))] ComponentData<LogicalOperator> logicalOperatorComponent
+    )
+    {
+        // Arrange
+        // Act
+        // Assert
+        Assert.Throws<ArgumentException>(() => new CompoundFilter(logicalOperatorComponent.Component, [Mock.Of<IFilter>()]));
+    }
+
+    [Test]
+    [MatrixDataSource]
+    public async Task ToString_WhenCalled_ExpectTheDefaultStringToBeReturned(
+        [MatrixMethod<ComponentDataDatasource>(nameof(ComponentDataDatasource.CompoundLogicalOperatorsDatasource))] ComponentData<LogicalOperator> logicalOperatorComponent,
+        [MatrixMethod<CompoundFilterTests>(nameof(RandomFiltersDatasource))] (Mock<IFilter> filter, string defaultString, string directoryServicesString, string ldapString)[] filters
+    )
+    {
+        // Arrange
+        CompoundFilter compoundFilter = new(logicalOperatorComponent.Component, filters.Select(f => f.filter.Object));
+        string expectedResults = $"({logicalOperatorComponent.Component}{string.Join(string.Empty, filters.Select(f => f.defaultString))})";
+
+        // Act
+        string actualResult = compoundFilter.ToString();
+
+        // Assert
+        await Assert.That(actualResult).IsEqualTo(expectedResults);
+    }
+
+    [Test]
+    [MatrixDataSource]
+    public async Task ToDirectoryServicesString_WhenCalled_ExpectTheDirectoryServicesEncodedStringToBeReturned(
+        [MatrixMethod<ComponentDataDatasource>(nameof(ComponentDataDatasource.CompoundLogicalOperatorsDatasource))] ComponentData<LogicalOperator> logicalOperatorComponent,
+        [MatrixMethod<CompoundFilterTests>(nameof(RandomFiltersDatasource))] (Mock<IFilter> filter, string defaultString, string directoryServicesString, string ldapString)[] filters
+    )
+    {
+        // Arrange
+        CompoundFilter compoundFilter = new(logicalOperatorComponent.Component, filters.Select(f => f.filter.Object));
+        string expectedResults = $"({logicalOperatorComponent.Component}{string.Join(string.Empty, filters.Select(f => f.directoryServicesString))})";
+
+        // Act
+        string actualResult = compoundFilter.ToDirectoryServicesString();
+
+        // Assert
+        await Assert.That(actualResult).IsEqualTo(expectedResults);
+    }
+
+    [Test]
+    [MatrixDataSource]
+    public async Task ToLdapString_WhenCalled_ExpectTheLdapEncodedStringToBeReturned(
+        [MatrixMethod<ComponentDataDatasource>(nameof(ComponentDataDatasource.CompoundLogicalOperatorsDatasource))] ComponentData<LogicalOperator> logicalOperatorComponent,
+        [MatrixMethod<CompoundFilterTests>(nameof(RandomFiltersDatasource))] (Mock<IFilter> filter, string defaultString, string directoryServicesString, string ldapString)[] filters
+    )
+    {
+        // Arrange
+        CompoundFilter compoundFilter = new(logicalOperatorComponent.Component, filters.Select(f => f.filter.Object));
+        string expectedResults = $"({logicalOperatorComponent.Component}{string.Join(string.Empty, filters.Select(f => f.ldapString))})";
+
+        // Act
+        string actualResult = compoundFilter.ToLdapString();
+
+        // Assert
+        await Assert.That(actualResult).IsEqualTo(expectedResults);
+    }
+
+    public static IEnumerable<(Mock<IFilter> filter, string defaultString, string directoryServicesString, string ldapString)[]> RandomFiltersDatasource()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            yield return [.. Enumerable.Range(0, i).Select(_ =>
+            {
+                string defaultString = Guid.NewGuid().ToString();
+                string directoryServicesString = Guid.NewGuid().ToString();
+                string ldapString = Guid.NewGuid().ToString();
+
+                Mock<IFilter> filterMock = new();
+                filterMock.Setup(f => f.ToString()).Returns(defaultString);
+                filterMock.Setup(f => f.ToDirectoryServicesString()).Returns(directoryServicesString);
+                filterMock.Setup(f => f.ToLdapString()).Returns(ldapString);
+
+                return (filterMock, defaultString, directoryServicesString, ldapString);
+            })];
+        }
+    }
+}
---
+++ Mittons.ActiveDirectory/Search/Filters/CompoundFilter.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mittons.ActiveDirectory.Search.Filters
+{
+    public class CompoundFilter : IFilter
+    {
+        public LogicalOperator LogicalOperator { get; }
+        public IEnumerable<IFilter> Filters { get; }
+
+        public CompoundFilter(LogicalOperator logicalOperator, IEnumerable<IFilter> filters)
+        {
+            if (logicalOperator.IsSimpleOperator)
+            {
+                throw new ArgumentException($"A CompoundFilter cannot be created with simple logical operator [{logicalOperator.Name}].", nameof(logicalOperator));
+            }
+
+            LogicalOperator = logicalOperator;
+            Filters = filters;
+        }
+
+        public override string ToString() => $"({LogicalOperator}{string.Join(string.Empty, Filters)})";
+        public string ToDirectoryServicesString() => $"({LogicalOperator}{string.Join(string.Empty, Filters.Select(f => f.ToDirectoryServicesString()))})";
+        public string ToLdapString() => $"({LogicalOperator}{string.Join(string.Empty, Filters.Select(f => f.ToLdapString()))})";
+    }
+}
